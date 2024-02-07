4908d5e0680c4c5cdd079c0f4aed14ced74564ce
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-07 06:58:16 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    function blurHandler(){
+        console.log('Hey! Eyes on me!')
+    }
+    function focusHandler(){
+        console.log('Good!')
+    }
+  return (
+    <div>
+      <button onBlur={blurHandler} onFocus={focusHandler}>Eyes on me</button>
+    </div>
+  )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
-// Code Keypad Component Here
+import React from 'react'
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
 
-export default Keypad;
+export default function Keypad() {
+    
+    function changeHandler(){
+        console.log('Entering password...')
+      } 
+      return (
+        <div>
+          <input type='password' onChange={changeHandler}/>
+        </div>
+      )
+    }
